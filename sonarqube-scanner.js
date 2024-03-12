bcca22afcd18ff9f24ee3a9fd1a44abc2b314b10
nandhinimano1 <nandhini.manoharan@kaviglobal.com>
2024-03-12 21:18:20 +0530

    sonarqube setup

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,15 +1,16 @@
-const scanner = require('sonarqube-scanner');
+const scanner = require("sonarqube-scanner");
 scanner(
   {
-    serverUrl: 'http://localhost:9000',
+    serverUrl: "http://localhost:9000",
+    login: "admin",
+    password: "admin1",
     options: {
-      'sonar.token': process.env.SONARQUBE_TOKEN,
-      'sonar.projectName': 'grandt',
-      'sonar.projectDescription': 'test sonarqube ...',
-      'sonar.sourceEncoding': 'UTF-8',
-      'sonar.sources': './src',
-      'sonar.exclusions': '**/*.test.tsx',
-      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+      "sonar.projectName": "grandt",
+      "sonar.projectDescription": "test sonarqube ...",
+      "sonar.sourceEncoding": "UTF-8",
+      "sonar.sources": "./src",
+      "sonar.exclusions": "**/*.test.tsx",
+      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
     },
   },
   () => process.exit()
